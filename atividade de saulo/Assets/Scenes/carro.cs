4b66e6d2c7727b03cabfbba738e6b76e90b43a07
Kaio-2112 <k.manoel@escolar.ifrn.edu.br>
2025-04-14 14:29:20 -0300

    inicio

---
+++ atividade de saulo/Assets/Scenes/carro.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class carro
+{
+    private string nome;
+    private string cor;
+    private int numero_de_rodas;
+    private float preço;
+
+    public void MudarNome(string nome)
+{
+    this.nome = nome;
+}
+
+public string
